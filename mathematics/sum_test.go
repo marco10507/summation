a4b8b3cdc8b850d2b76b44f491880cac25b9a066
marco.burgos@expereo.com <marco.burgos>
2022-03-07 23:02:49 +0100

    benchmark

--- mathematics/sum_test.go
+++ mathematics/sum_test.go
@@ -1,6 +1,9 @@
 package mathematics
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 type test struct {
 	data     []float64
@@ -11,6 +14,17 @@ var tests = []test{
 	{[]float64{1}, 1},
 	{[]float64{1, 2}, 3},
 	{[]float64{1, 2, 3}, 6},
+}
+
+var items = createItems(1000000)
+
+func createItems(number int) []float64 {
+	items := make([]float64, number)
+	for i := 0; i < number; i++ {
+		items[i] = float64(i)
+	}
+
+	return items
 }
 
 func TestSum1(t *testing.T) {
@@ -28,5 +42,27 @@ func TestSum2(t *testing.T) {
 		if sum != test.expected {
 			t.Error("Expected", test.expected, "Actual", sum)
 		}
+	}
+}
+
+func ExampleSum1() {
+	fmt.Println(Sum1([]float64{1, 2}))
+	// Output: 3
+}
+
+func ExampleSum2() {
+	fmt.Println(Sum2([]float64{1, 2}))
+	// Output: 3
+}
+
+func BenchmarkSum1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sum1(items)
+	}
+}
+
+func BenchmarkSum2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sum2(items)
 	}
 }
